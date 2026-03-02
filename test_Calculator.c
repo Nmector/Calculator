98c2eb85a36e958459d5cb992a110642859d38b4
Nehemiah Ector <nmector@cpp.edu>
2026-03-02 13:40:26 -0800

    Failed test

--- test_Calculator.c
+++ test_Calculator.c
@@ -5,7 +5,7 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_add_positive_numbers(void) {
-    TEST_ASSERT_EQUAL(5, add(2, 3));
+    TEST_ASSERT_EQUAL(6, add(2, 3));
 }
 
 int main(void) {
